8f5e05bda91b000ead84208ee398e3e27f75a4ac
Aceep <alycia.gautier@laposte.net>
2024-06-11 12:07:30 +0200

    01

---
+++ C/Colleen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Here it's the first comment
+
+void    print(char *program) {
+    printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, program, 10, 10, 10, 10, 10);
+}
+
+int main() {
+    char *program = "#include <stdio.h>%c%c//Here it's the first comment%c%cvoid    print(char *program) {%c    printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, program, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    char *program = %s;%c    print(program);%c//Here it's the second comment%c    return 0;%c}%c";
+    print(program);
+    //Here it's the second comment
+    return 0;
+}
