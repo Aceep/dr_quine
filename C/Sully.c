cca186ac2e4b7e4391adbe9ab6af752aef74985b
Aceep <alycia.gautier@laposte.net>
2024-06-12 11:59:48 +0200

    C part

--- C/Sully.c
+++ C/Sully.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILENAME "Sully_%d.c"
+#define CONTENT "#include <stdio.h>%c#include <stdlib.h>%c%c#define FILENAME %c%s%c%c#define CONTENT %c%s%c%c%cint main() {%c    int i = %d;%c    if (i <= 0) return (0);%c    char filename[100];%c    sprintf(filename, FILENAME, --i);%c    FILE *file = fopen(filename, %cw%c);%c    fprintf(file, CONTENT, 10, 10, 10, 34, FILENAME, 34, 10, 34, CONTENT, 34, 10, 10, 10, i, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10);%c    fclose(file);%c    char command[100];%c    sprintf(command, %cgcc Sully_%%d.c -o Sully_%%d%c, i, i);%c    system(command);%c    sprintf(command, %c./Sully_%%d%c, i);%c    system(command);%c}"
+
 int main() {
-    return 0;
+    int i = 5;
+    if (i <= 0) return (0);
+    char filename[100];
+    sprintf(filename, FILENAME, --i);
+    FILE *file = fopen(filename, "w");
+    fprintf(file, CONTENT, 10, 10, 10, 34, FILENAME, 34, 10, 34, CONTENT, 34, 10, 10, 10, i, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10);
+    fclose(file);
+    char command[100];
+    sprintf(command, "gcc Sully_%d.c -o Sully_%d", i, i);
+    system(command);
+    sprintf(command, "./Sully_%d", i);
+    system(command);
 }
--- C/test.sh
+++ C/test.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
 if [ "$1" = "Colleen" ]; then
+    echo -e "Test Colleen\nRegister the output of the program in a file and compare it with the original file."
     ./Colleen > tmp_Colleen
-    diff tmp_Colleen Colleen.c > tmp_Diff
-    if [ -s tmp_Diff ] && [ -e tmp_Diff ]
+    diff tmp_Colleen Colleen.c > tmp_Colleen_2
+    echo -ne "diff the output(tmp_Colleen) and the original file(Colleen.c).\nThey should be identical : "
+    if [ -s tmp_Colleen_2 ] && [ -e tmp_Colleen_2 ]
     then
-        echo "KO"
+        echo -e "KO\n"
     else
-        echo "OK"
+        echo -e "OK\n"
     fi
 fi
 
 if [ "$1" = "Grace" ]; then
+    echo -e "Test Grace\nGrace should create a file named Grace_kid.c which should be the same as the original file."
     ./Grace
     diff Grace.c Grace_kid.c > tmp_Grace
+    echo -ne "diff the output(Grace_kid.c) and the original file(Grace.c).\nThey should be identical : "
     if [ -s tmp_Grace ] && [ -e tmp_Grace ]
     then
-        echo "KO"
+        echo -e "KO\n"
     else
-        echo "OK"
+        echo -e "OK\n"
     fi
 fi
 
 if [ "$1" = "Sully" ]; then
-    ./Sully > tmp_Sully
-    diff Sully.c tmp_Sully > tmp_Diff2
-    if [ -s tmp_Diff ] && [ -e tmp_Diff2 ]
+    echo -e "Test Sully\nSully should create 13 files named Sully_X.c where X is the number of files created."
+    if [ -e tmp_3 ]
     then
-        echo "KO"
-    else
+        rm tmp_3
+    fi
+    ./Sully
+    LS=$(ls -l | grep Sully | wc -l)
+    COUNT=4
+    diff Sully.c Sully_4.c >> tmp_3
+    while [ $COUNT -gt 0 ]; do
+        diff Sully_$COUNT.c Sully_$((COUNT - 1)).c >> tmp_3
+        COUNT=$((COUNT - 1))
+    done
+    nombre_lignes=$(wc -l < tmp_3)
+    echo -ne "diff the output(Sully_X.c) and the original file(Sully.c).\nThe number of lines should be 20 and the number of files should be 13 : "
+    if [ $nombre_lignes -eq 20 ] && [ $LS -eq 13 ]
+    then
         echo "OK"
+    else
+        echo "KO"
     fi
 fi
 
