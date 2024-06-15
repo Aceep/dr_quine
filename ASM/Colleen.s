0b14079a306811676d6545cc0c1503bdcd06c934
Aceep <alycia.gautier@laposte.net>
2024-06-15 15:39:49 +0200

    colleen augmented version

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -4,19 +4,29 @@ global main
 extern printf
 
 section .data
-format db "; Outside comment%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .data%1$cformat db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$center 0, 0%1$clea rdi, [rel format]%1$c; Inside comment%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel format]%1$ccall printf%1$cleave%1$ccall return%1$c%1$creturn:%1$cret%1$c", 0
+format db "; Outside comment%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .data%1$cformat db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c%1$ccolleen:%1$cpush rbp%1$cmov rbp, rsp%1$clea rdi, [rel format]%1$c; Inside comment%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel format]%1$ccall printf%1$cmov rsp, rbp%1$cpop rbp%1$cret%1$c%1$cmain:%1$cpush rbp%1$cmov rbp, rsp%1$ccall colleen%1$cmov rsp, rbp%1$cpop rbp%1$ccall return%1$c%1$creturn:%1$cret%1$c", 0
 
 section .text
 
-main:
-enter 0, 0
+colleen:
+push rbp
+mov rbp, rsp
 lea rdi, [rel format]
 ; Inside comment
 mov rsi, 10
 mov rdx, 34
 lea rcx, [rel format]
 call printf
-leave
+mov rsp, rbp
+pop rbp
+ret
+
+main:
+push rbp
+mov rbp, rsp
+call colleen
+mov rsp, rbp
+pop rbp
 call return
 
 return:
