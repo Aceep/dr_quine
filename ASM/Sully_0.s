63bc9e77266189553ad8c631f390a0e0ae6c8555
Aceep <alycia.gautier@laposte.net>
2024-06-16 15:02:22 +0200

    need to clean Sully's code

--- ASM/Sully_0.s
+++ ASM/Sully_0.s
@@ -2,21 +2,22 @@ section .data
     info:   db "section .data%1$c    info:   db %2$c%3$s%2$c,0%1$c    file:   db %2$cSully_X.s%2$c,0%1$c    cmd:	db %2$cnasm -f elf64 Sully_X.s; clang -no-pie Sully_X.o -o Sully_X; ./Sully_X%2$c,0%1$c    iter:	db %4$d%1$c%1$csection .text%1$c    global main%1$c    extern open, close, dprintf, chmod, system%1$c%1$cmain:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	mov rax, [rel iter]%1$c	sub rax, 1%1$c	cmp rax, 47 ; if value is less then 0, quit%1$c	jle quitsully%1$c	mov r9, rax%1$c%1$c	lea rdi, [rel file]%1$c	add rdi, 6%1$c	stosb%1$c	lea rdi, [rel cmd]%1$c	add rdi, 20%1$c	stosb%1$c	add rdi, 24%1$c	stosb%1$c	add rdi, 12%1$c	stosb%1$c	add rdi, 10%1$c	stosb%1$c%1$c	lea rdi, [rel file]%1$c	mov rsi, 0q1101%1$c	call open%1$c%1$c	mov r8, rax ; temp move fd to r8%1$c	lea rdi, [rel file]%1$c	mov rsi, 0q0777%1$c	call chmod%1$c%1$c	mov rdi, r8%1$c	lea rsi, [rel info]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel info]%1$c	call dprintf%1$c%1$c	call close%1$c%1$c	lea rdi, [rel cmd]%1$c	call system%1$c%1$cquitsully:%1$c    pop rbp%1$c    mov rax, 0%1$c    ret%1$c",0
     file:   db "Sully_X.s",0
     cmd:	db "nasm -f elf64 Sully_X.s; clang -no-pie Sully_X.o -o Sully_X; ./Sully_X",0
-    iter:	db 53
+    iter:	db 48
 
 section .text
     global main
     extern open, close, dprintf, chmod, system
 
-decrement_iter:
-    mov rax, [rel iter]
-    sub rax, 1
-    cmp rax, 47
-    jle quitsully
-    mov r9, rax
-    ret
+main:
+	push rbp
+	mov rbp, rsp
 
-replace_index:
+	mov rax, [rel iter]
+	sub rax, 1
+	cmp rax, 47 ; if value is less then 0, quit
+	jle quitsully
+	mov r9, rax
+
 	lea rdi, [rel file]
 	add rdi, 6
 	stosb
@@ -29,31 +30,17 @@ replace_index:
 	stosb
 	add rdi, 10
 	stosb
-    ret
 
-open_file:
-    lea rdi, [rel file]
+	lea rdi, [rel file]
 	mov rsi, 0q1101
 	call open
-    ret
 
-main:
-	push rbp
-	mov rbp, rsp
-
-    call decrement_iter
-
-    call replace_index
-
-    call open_file
-
-    call close
+	mov r8, rax ; temp move fd to r8
 	lea rdi, [rel file]
 	mov rsi, 0q0777
 	call chmod
 
-    call open_file
-	mov rdi, rax
+	mov rdi, r8
 	lea rsi, [rel info]
 	mov rdx, 10
 	mov rcx, 34
@@ -69,5 +56,3 @@ quitsully:
     pop rbp
     mov rax, 0
     ret
-
-
--- ASM/test.sh
+++ ASM/test.sh
@@ -19,7 +19,7 @@ if [ "$1" = "Grace" ]; then
 fi
 
 if [ "$1" = "Sully" ]; then
-    echo -e "Test Sully\nSully should create 13 files named Sully_X.c where X is the number of files created."
+    echo -e "Test Sully\nSully should create 13 files named Sully_X.s where X is the number of files created."
     if [ -e tmp_3 ]
     then
         rm tmp_3
@@ -27,18 +27,19 @@ if [ "$1" = "Sully" ]; then
     ./Sully
     LS=$(ls -l | grep Sully | wc -l)
     COUNT=4
-    diff Sully.c Sully_4.c >> tmp_3
+    diff Sully.s Sully_4.s >> tmp_3
     while [ $COUNT -gt 0 ]; do
-        diff Sully_$COUNT.c Sully_$((COUNT - 1)).c >> tmp_3
+        diff Sully_$COUNT.s Sully_$((COUNT - 1)).s >> tmp_3
         COUNT=$((COUNT - 1))
     done
     nombre_lignes=$(wc -l < tmp_3)
-    echo -ne "diff the output(Sully_X.c) and the original file(Sully.c).\nThe number of lines should be 20 and the number of files should be 13 : "
+    echo -ne "diff the output(Sully_X.s) and the original file(Sully.s).\nThe number of lines should be 20 and the number of files should be 13 : "
     if [ $nombre_lignes -eq 20 ] && [ $LS -eq 13 ]
     then
         echo "OK"
     else
         echo "KO"
+        echo "The number of lines is $nombre_lignes and the number of files is $LS"
     fi
 fi
 
